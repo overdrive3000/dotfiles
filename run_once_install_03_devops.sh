0392a7f109d9aaf8366fcf82627ceb8183814522
Juan Mesa <linuxven@gmail.com>
2022-01-30 09:37:12 +0100

    Add krew installation

--- run_once_install_03_devops.sh
+++ run_once_install_03_devops.sh
@@ -56,3 +56,15 @@ sudo apt update
 sudo apt install -y docker-ce docker-ce-cli containerd.io
 
 sudo usermod -aG docker ${USER}
+
+# Install krew kubectl package manager
+(
+  set -x; cd "$(mktemp -d)" &&
+  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+  KREW="krew-${OS}_${ARCH}" &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
+  tar zxvf "${KREW}.tar.gz" &&
+  ./"${KREW}" install krew
+)
+
